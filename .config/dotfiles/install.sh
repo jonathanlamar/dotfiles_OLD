661818747a2192bc386c3cd5c2533db3ae7c7b22
jon <jonathan.p.lamar@gmail.com>
2020-05-16 16:03:11 -0600

    Giving up and trying an install shell script.

---
+++ .config/dotfiles/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+read -p "Enter your username: " USERNAME
+read -p "Enter your password: " PASSWD
+
+# Install apt dependencies
+yes $PASSWD | sudo apt install exuberant-ctags git golang-go neovim nodejs npm openjdk-8-jdk python3-gi python3-pip python3-xlib ripgrep xsel yarn zsh
+
+# Install pip dependencies
+pip3 install pylint jedi virtualenv
+
+# Install pygrid
+./pygrid.sh $USERNAME
+
+# Change default shell to fzf
+yes $PASSWD | chsh -s /bin/zsh
+
+# Install oh-my-zsh
+./ohMyZsh.sh $USERNAME
+
+# Install powerline fonts
+./powerline.sh $USERNAME
+
+# Install fzf
+./fzf.sh $USERNAME
+
+# Install neovim
+./neovim.sh $USERNAME
